4d1456366d1737966e127e958d2d42ad75f3e755
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-03-25 14:41:41 +0100

    Register functionality

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,6 +1,7 @@
-import { StyleSheet, View, ScrollView, Switch, Text } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
+import { useDispatch } from 'react-redux';
 
 import CustomInput from '../components/CustomInput';
 import CustomButton from '../components/CustomButton';
@@ -8,18 +9,45 @@ import CustomSwitch from '../components/CustomSwitch';
 import TitleText from '../components/TitleText';
 import CustomColors from '../constans/Colors';
 import CustomDimensions from '../constans/Dimensions';
+import allergyBackend from '../api/allergyBackend';
+import { login } from '../store/actions/auth';
 
 const SignUpScreen = () => {
 	const [ username, setUsername ] = useState('');
+	const [ email, setEmail ] = useState('');
 	const [ password, setPassword ] = useState('');
 	const [ passwordRepeat, setPasswordRepeat ] = useState('');
 	const [ pwz, setPwz ] = useState(''); // TODO: 7 cyfr
 	const [ isDoctor, setIsDoctor ] = useState(false);
 
 	const navigation = useNavigation();
+	const dispatch = useDispatch();
 
-	const onRegisterPressed = () => {
-		// TODO: Rejestracja i Go to Choose Allergies
+	const validateFields = () => {
+		//TODO: Show Error
+		if (!username) {
+			return false;
+		}
+		if (!email) {
+			return false;
+		}
+		if (!password || !passwordRepeat || password.length < 8 || !password === passwordRepeat) {
+			return false;
+		}
+		if (isDoctor && !pwz) {
+			return false;
+		}
+	};
+
+	const onRegisterPressed = async () => {
+		if (validateFields) {
+			let registerData = { login: username, password, pwz, email };
+			let response = await allergyBackend.post('/users', registerData);
+			const { user, token } = response.data;
+			dispatch(login(user, token));
+			// TODO: GOTO choose allergies
+			return navigation.navigate('Home');
+		}
 	};
 
 	const onSignInPressed = () => {
@@ -35,6 +63,7 @@ const SignUpScreen = () => {
 			<View style={styles.root}>
 				<TitleText style={styles.title}>Rejestracja</TitleText>
 				<CustomInput placeholder="Login" value={username} setValue={setUsername} />
+				<CustomInput placeholder="Email" value={email} setValue={setEmail} />
 				<CustomInput placeholder="Hasło" value={password} setValue={setPassword} secureTextEntry />
 				<CustomInput
 					placeholder="Powtórz hasło"
